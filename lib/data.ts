a6a356eaf30ddf1188ff8c6340d4568888b6d212
Kenjiisok <dudu.hirose@hotmail.com>
2024-07-02 16:37:55 -0300

    initial commit

---
+++ lib/data.ts
@@ -0,0 +1,69 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import portfolioImg from "@/public/portifolioprint.png"
+import starbucksImg from "@/public/starbucksprint.png"
+import weatherImg from "@/public/weather_app.png"
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "Sobre",
+    hash: "#about",
+  },
+  {
+    name: "Projetos",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experiência",
+    hash: "#experience",
+  },
+  {
+    name: "Contato",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Graduated bootcamp",
+    location: "Miami, FL",
+    description:
+      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2019",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "CorpComment",
+    description:
+      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
+    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+    // imageUrl: corpcommentImg,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "GraphQL",
+  "Framer Motion",
+] as const;
