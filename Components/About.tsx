55a9e83e91b17b54963eb30b53f5da685f17bb3f
Kenjiisok <dudu.hirose@hotmail.com>
2024-07-03 15:59:53 -0300

    Add. section divider e about me section

---
+++ Components/About.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Image from "next/image";
+import corpoImg from "@/public/corpo copiar.png";
+
+export default function About() {
+  return (
+    <section>
+      <h2>Sobre mim</h2>
+    </section>
+  );
+}
